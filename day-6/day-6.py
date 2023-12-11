637872bd8db4fb9e59812eedca481763f54186e1
MaximoRdz <mxrdhr@gmail.com>
2023-12-11 12:33:46 +0100

    part 1 solved

--- day-6/day-6.py
+++ day-6/day-6.py
@@ -1,5 +1,5 @@
 import math
-
+import numpy as np
 
 def second_order(a, b, c):
     radicant = b**2 - 4*a*c
@@ -7,8 +7,15 @@ def second_order(a, b, c):
         return (-b - math.sqrt(radicant))/(2*a), (-b + math.sqrt(radicant))/(2*a)
     else: 
         return None
+    
+    
+def distance(dt, T):
+    return dt*(T-dt)
+distance = np.vectorize(distance)
 
-with open("day-6/example.txt", "r") as file:
+
+
+with open("day-6/input.txt", "r") as file:
     data = file.read().split("\n")
 
 
@@ -17,9 +24,11 @@ R_array = [int(r) for r in data[1].split(":")[1].split()]
 
 ans = 1
 for T, R in zip(T_array, R_array):
+
     sol_1, sol_2 = second_order(1, -T, R)
-    ans *= (sol_2-1) - sol_1
-    print("sol 1:", sol_1, "sol 2:", sol_2, " -> ", (sol_2-1) - sol_1)
+    t_arr = np.arange(math.floor(sol_1), math.ceil(sol_2)+1)
+    x_arr = distance(t_arr, T)
+    ans *= len(x_arr[(x_arr > R)])
 
 print("Part 1: ", ans)
 
