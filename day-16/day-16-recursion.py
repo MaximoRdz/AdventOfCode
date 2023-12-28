8eb5ae7561bd06f3ea1f6c2c434cdf8de027c78d
MaximoRdz <mxrdhr@gmail.com>
2023-12-28 13:39:09 +0100

    day-16 solved

---
+++ day-16/day-16-recursion.py
@@ -0,0 +1,88 @@
+import sys 
+sys.setrecursionlimit(2000)
+print(sys.getrecursionlimit())
+
+assert False
+
+with open("day-16/input.txt", "r") as file:
+    matrix = file.read().split("\n")
+
+
+grid = [[s for s in row] for row in matrix]
+NROWS = len(grid)
+NCOLS = len(grid[0])
+visited = [[" " for j in range(NCOLS)] for i in range(NROWS)]
+
+# print(*grid, sep="\n")
+
+# Beam initial coordinates (i, j)
+# Beam direction
+#   - row left (0, -1)
+#   - row right (0, 1)
+#   - column up (-1, 0)
+#   - column down (1, 0)
+
+cache = set()
+def beam_ray(beam_i, beam_j, dir_i, dir_j):
+    key = (beam_i, beam_j, dir_i, dir_j)
+    if key in cache:
+        return None
+    
+    cache.add(key)
+
+    next_i, next_j = beam_i+dir_i, beam_j+dir_j
+    if not (0<=next_i<NROWS and 0<=next_j<NCOLS):
+        return None
+    
+    visited[next_i][next_j] = "#"
+    next_tile = grid[next_i][next_j]
+
+    if next_tile == ".":
+        aux = beam_ray(next_i, next_j, dir_i, dir_j)
+    elif next_tile == "-":
+        if dir_i == 0:
+            aux = beam_ray(next_i, next_j, dir_i, dir_j)
+        else:
+            aux = beam_ray(next_i, next_j, 0, 1)
+            aux = beam_ray(next_i, next_j, 0, -1)
+    elif next_tile == "|":
+        if dir_i == 0:
+            aux = beam_ray(next_i, next_j, 1, 0)
+            aux = beam_ray(next_i, next_j, -1, 0)
+        else:
+            aux = beam_ray(next_i, next_j, dir_i, dir_j)
+    elif next_tile == "\\":
+        aux = beam_ray(next_i, next_j, dir_j, dir_i)
+    elif next_tile == "/":
+        aux = beam_ray(next_i, next_j, -dir_j, -dir_i)
+    else:
+        return None
+    
+    return None
+
+def visited_tiles(visited):
+    ans = 0
+    for row in visited:
+        for col in row:
+            ans += 0 if col==" " else 1
+    return ans
+
+visited[0][0] = "#"
+# beam_ray(0, 0, 0, 1)
+# print("Solution Part 1: ", visited_tiles(visited))
+
+beam_ray(0, 0, 1, 0)
+print("Solution Part 1: ", visited_tiles(visited))
+# print(cache)
+# print(*visited, sep="\n")
+
+
+    
+
+
+
+
+
+
+
+    
--- day-16/day-16.py
+++ day-16/day-16.py
@@ -1,15 +1,12 @@
+from collections import deque
 
 
-with open("day-16/example.txt", "r") as file:
+with open("day-16/input.txt", "r") as file:
     matrix = file.read().split("\n")
-
 
 grid = [[s for s in row] for row in matrix]
 NROWS = len(grid)
 NCOLS = len(grid[0])
-visited = [[False for j in range(NCOLS)] for i in range(NROWS)]
-
-print(*grid, sep="\n")
 
 # Beam initial coordinates (i, j)
 # Beam direction
@@ -18,42 +15,50 @@ print(*grid, sep="\n")
 #   - column up (-1, 0)
 #   - column down (1, 0)
 
-def beam_ray(beam_i, beam_j, dir_i, dir_j):
+beam_i, beam_j = 0, 0
+dir_i, dir_j = 1, 0
+
+visited = set()
+visited.add((beam_i, beam_j))
+
+cache = set()
+tile_queue = deque()
+tile_queue.append((beam_i, beam_j, dir_i, dir_j))
+
+while len(tile_queue):
+    beam_i, beam_j, dir_i, dir_j = tile_queue.popleft()
+    key = (beam_i, beam_j, dir_i, dir_j)
+    if key in cache:
+        continue
+    cache.add(key)
+
     next_i, next_j = beam_i+dir_i, beam_j+dir_j
     if not (0<=next_i<NROWS and 0<=next_j<NCOLS):
-        return None
-    
-    visited[next_i][next_j] = True
+        continue
+
+    visited.add((next_i, next_j))
     next_tile = grid[next_i][next_j]
 
     if next_tile == ".":
-        beam_ray(next_i, next_j, dir_i, dir_j)
+        tile_queue.append((next_i, next_j, dir_i, dir_j))
     elif next_tile == "-":
         if dir_i == 0:
-            beam_ray(next_i, next_j, dir_i, dir_j)
+            tile_queue.append((next_i, next_j, dir_i, dir_j))
         else:
-            beam_ray(next_i, next_j, 0, 1)
-            beam_ray(next_i, next_j, 0, -1)
+            tile_queue.append((next_i, next_j, 0, 1))
+            tile_queue.append((next_i, next_j, 0, -1))
     elif next_tile == "|":
         if dir_i == 0:
-            beam_ray(next_i, next_j, 1, 0)
-            beam_ray(next_i, next_j, -1, 0)
+            tile_queue.append((next_i, next_j, 1, 0))
+            tile_queue.append((next_i, next_j, -1, 0))
         else:
-            beam_ray(next_i, next_j, dir_i, dir_j)
+            tile_queue.append((next_i, next_j, dir_i, dir_j))
     elif next_tile == "\\":
-        pass
+        tile_queue.append((next_i, next_j, dir_j, dir_i))
     elif next_tile == "/":
-        pass
+        tile_queue.append((next_i, next_j, -dir_j, -dir_i))
     else:
-        return None
+        continue
 
-    
-    
-
-
-
-
-
-
-
+print("Solution Part 1: ", len(visited))
     
