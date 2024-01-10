b97860b2d25d59cb99bcdb405ae8e4bd6689682e
MaximoRdz <mxrdhr@gmail.com>
2024-01-10 15:38:48 +0100

    even and odd grids bug

--- day-21/day-21-part-2.py
+++ day-21/day-21-part-2.py
@@ -25,9 +25,9 @@ assert contained_grids % 2 == 1, "There is and even number of grids in the max a
 print((contained_grids // 2 + 1) * N)
 
 def reach(i0, j0, max_steps):
-    q = deque([(i0, j0, 0)]) # state i, j, steps
+    q = deque([(i0, j0, 0)]) # state: i, j, steps
     garden_plots = {(i0, j0)}
-    reached = {(i0, j0)}
+    reached = set()
 
     while q:
         ci, cj, step = q.popleft()
@@ -61,36 +61,11 @@ def reach(i0, j0, max_steps):
 # inner grids: 
 #     * 
 
-# def periodic_boundaries(i, j):
-#     # a // b = 0 a<b
-#     # a // b = 1 b<=a<2b
-#     # . . . 
-#     ni = i - (i//N)*N
-#     nj = j - (j//N)*N
-#     return ni, nj 
+print("Start Square: ", START)
+even = reach(*START, 2*N)
+print("Allowed states with even steps: ", even)
+odd = reach(*START, 1)
+print("Allowed states with odd steps: ", odd)
 
-# q = deque([(*START, 0)]) # state i, j, steps
-# garden_plots = {START}
-# visited = {START}
-
-# MAX_STEPS = 1000
-# while q:
-#     ci, cj, step = q.popleft()
-
-#     if step % 2 == 0:
-#         garden_plots.add((ci, cj))
-
-#     if step == MAX_STEPS:
-#         continue
-
-#     for di, dj in [(0, -1), (-1, 0), (0, 1), (1, 0)]:
-#         ni, nj = ci + di, cj + dj
-#         npi, npj = periodic_boundaries(ni, nj)
-#         if (ni, nj) in visited or grid[npi][npj] == "#":
-#             continue
-#         q.append((ni, nj, step+1))
-#         visited.add((ni, nj))
-
-  
-# print("Solution Part 2: ", len(garden_plots))
+"""I'm missing something even and odd scenarios must be different..."""
 
