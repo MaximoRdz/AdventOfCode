e731367c03878277ca03d731ba4daced0dc7eaca
MaximoRdz <mxrdhr@gmail.com>
2024-01-11 08:52:34 +0100

    odd and even solved step count down instead of count up

--- day-21/day-21-part-2.py
+++ day-21/day-21-part-2.py
@@ -25,7 +25,7 @@ assert contained_grids % 2 == 1, "There is and even number of grids in the max a
 print((contained_grids // 2 + 1) * N)
 
 def reach(i0, j0, max_steps):
-    q = deque([(i0, j0, 0)]) # state: i, j, steps
+    q = deque([(i0, j0, max_steps)]) # state: i, j, steps
     garden_plots = {(i0, j0)}
     reached = set()
 
@@ -35,7 +35,7 @@ def reach(i0, j0, max_steps):
         if step % 2 == 0:
             garden_plots.add((ci, cj))
 
-        if step == max_steps:
+        if step == 0:
             continue
 
         for di, dj in [(0, -1), (-1, 0), (0, 1), (1, 0)]:
@@ -45,7 +45,7 @@ def reach(i0, j0, max_steps):
                 continue
             if (ni, nj) in reached:
                 continue
-            q.append((ni, nj, step+1))
+            q.append((ni, nj, step-1))
             reached.add((ni, nj))
 
     return len(garden_plots)
@@ -61,11 +61,9 @@ def reach(i0, j0, max_steps):
 # inner grids: 
 #     * 
 
-print("Start Square: ", START)
 even = reach(*START, 2*N)
+odd = reach(*START, 2*N+1)
 print("Allowed states with even steps: ", even)
-odd = reach(*START, 1)
 print("Allowed states with odd steps: ", odd)
 
-"""I'm missing something even and odd scenarios must be different..."""
 
